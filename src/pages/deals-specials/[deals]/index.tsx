d8188b1af15fb265ca4cc11fe845e599d50f0480
Uguumur Lkhagvadorj <iluguumur@gmail.com>
2024-06-06 12:59:19 +0800

    up

--- src/pages/deals-specials/[deals]/index.tsx
+++ src/pages/deals-specials/[deals]/index.tsx
@@ -1,3 +1,4 @@
+import Questions from "@/modules/layout/components/questions";
 import PageHeader from "@modules/layout/components/page-header"
 import axios from "axios";
 import https from "https";
@@ -69,50 +70,7 @@ const New = () => {
                 </div>
             </main>
             <aside className="page-sidebar  col-md-3 col-md-pull-9">
-              <div className="widget-even widget-2 widget widget_black_studio_tinymce" id="black-studio-tinymce-2">
-                  <h6 className="heading-title accent">
-                  <b>Questions?</b>
-                  <span>Get In Touch Today</span>
-                  </h6>
-                  <div className="textwidget">
-                    <div className="wpcf7 js" id="wpcf7-f233-o1" lang="en-US" dir="ltr">
-                      <div className="screen-reader-response">
-                        <p role="status" aria-live="polite" aria-atomic="true"></p>
-                        <ul></ul>
-                      </div>
-                      <form action="/deals-specials/#wpcf7-f233-o1" method="post" className="wpcf7-form init" id="sidebarForm" aria-label="Contact form" data-status="init">
-                        <div className="row">
-                          <div className="col-xs-6 col-md-12 form-row">
-                            <label>Your Name*</label>
-                            <span className="wpcf7-form-control-wrap" data-name="your-name">
-                              <input className="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" type="text" name="your-name"/>
-                            </span>
-                          </div>
-                          <div className="col-xs-6 col-md-12 form-row">
-                            <label>Email*</label>
-                            <span className="wpcf7-form-control-wrap" data-name="email">
-                              <input  className="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email" aria-required="true" aria-invalid="false" type="email" name="email"/>
-                            </span>
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="col-md-12 form-row">
-                            <label>Message</label>
-                            <span className="wpcf7-form-control-wrap" data-name="message">
-                              <textarea className="wpcf7-form-control wpcf7-textarea textarea-short" aria-invalid="false" name="message"></textarea>
-                            </span>
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="col-xs-6 col-md-12 form-row">
-                            <button className="button button--primary button--block" type="submit">Submit</button>
-                          </div>
-                        </div>
-                        <div className="wpcf7-response-output" aria-hidden="true"></div>
-                      </form>
-                    </div>
-                  </div>
-              </div>
+              <Questions/>
             </aside>
           </div>
         </article>
@@ -124,7 +82,7 @@ export default New
 
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const id = context.params?.id
+  const id = context.params?.deals
   const instance = axios.create({
     httpsAgent: new https.Agent({  
       rejectUnauthorized: false
--- src/pages/deals-specials/index.tsx
+++ src/pages/deals-specials/index.tsx
@@ -1,14 +1,17 @@
 import PageHeader from "@modules/layout/components/page-header"
 import { GetServerSideProps, GetStaticPropsContext, InferGetServerSidePropsType } from "next";
 import axios from 'axios';
+import https from "https";
+import Questions from "@/modules/layout/components/questions";
+import { useRouter } from "next/router";
 
 type Repo = {
   name: string
 }
  
-export default function Deals({
-  repo,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+const DealsPage: InferGetServerSidePropsType<typeof getServerSideProps> = (props: any) => {
+  const deals = props.data;
+  const {locale, locales, route, asPath} = useRouter();
   return (
     <>
         <PageHeader title="deals & specials"/>
@@ -16,60 +19,19 @@ export default function Deals({
           <div className="row">
             <main className="page-content col-md-9 col-md-push-3 specials-deals">
               <div className="row">
-                  <div className="col-sm-6 box-deals">
-                  <a href="/deals-specials/newcustomer" className="deal" id="deal-97908">
-                      <h2 className="post__title"> NEW CUSTOMER OFFER </h2>
-                      <img width="2088" height="1046" src="https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2.jpg" className="img-responsive wp-post-image entered lazyloaded" alt="" decoding="async" fetchPriority="high" data-lazy-srcset="https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2.jpg 2088w, https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2-300x150.jpg 300w, https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2-1024x513.jpg 1024w, https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2-768x385.jpg 768w, https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2-1536x769.jpg 1536w, https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2-2048x1026.jpg 2048w" data-lazy-sizes="(max-width: 2088px) 100vw, 2088px" data-lazy-src="https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2.jpg" data-ll-status="loaded" sizes="(max-width: 2088px) 100vw, 2088px" srcSet="https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2.jpg 2088w, https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2-300x150.jpg 300w, https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2-1024x513.jpg 1024w, https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2-768x385.jpg 768w, https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2-1536x769.jpg 1536w, https://thompsonmachinery.com/content/uploads/2020/06/MicrosoftTeams-image-2-2048x1026.jpg 2048w"/>
-                      <button className="button button--primary button--block"> Read More </button>
-                  </a>
+              {deals.map((item:any, index: any) => ( 
+                  <div key={index} className="col-sm-6 box-deals">
+                    <a href={`/deals-specials/${item.id}`} className="deal">
+                        <h2 className="post__title"> {locale === "mn" ? item.title : item.title_en} </h2>
+                        <img width="2088" height="1046" src={'https://webapi.barloworld.mn/file/'+item.img_path}className="alignleft img-responsive wp-post-image" alt="" decoding="async" sizes="(max-width: 300px) 100vw, 300px" /> 
+                        <button className="button button--primary button--block"> Read More </button>
+                    </a>
                   </div>
+              ))}
               </div>
             </main>
             <aside className="page-sidebar  col-md-3 col-md-pull-9">
-              <div className="widget-even widget-2 widget widget_black_studio_tinymce" id="black-studio-tinymce-2">
-                  <h6 className="heading-title accent">
-                  <b>Questions?</b>
-                  <span>Get In Touch Today</span>
-                  </h6>
-                  <div className="textwidget">
-                    <div className="wpcf7 js" id="wpcf7-f233-o1" lang="en-US" dir="ltr">
-                      <div className="screen-reader-response">
-                        <p role="status" aria-live="polite" aria-atomic="true"></p>
-                        <ul></ul>
-                      </div>
-                      <form action="/deals-specials/#wpcf7-f233-o1" method="post" className="wpcf7-form init" id="sidebarForm" aria-label="Contact form" data-status="init">
-                        <div className="row">
-                          <div className="col-xs-6 col-md-12 form-row">
-                            <label>Your Name*</label>
-                            <span className="wpcf7-form-control-wrap" data-name="your-name">
-                              <input className="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" type="text" name="your-name"/>
-                            </span>
-                          </div>
-                          <div className="col-xs-6 col-md-12 form-row">
-                            <label>Email*</label>
-                            <span className="wpcf7-form-control-wrap" data-name="email">
-                              <input  className="wpcf7-form-control wpcf7-email wpcf7-validates-as-required wpcf7-text wpcf7-validates-as-email" aria-required="true" aria-invalid="false" type="email" name="email"/>
-                            </span>
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="col-md-12 form-row">
-                            <label>Message</label>
-                            <span className="wpcf7-form-control-wrap" data-name="message">
-                              <textarea className="wpcf7-form-control wpcf7-textarea textarea-short" aria-invalid="false" name="message"></textarea>
-                            </span>
-                          </div>
-                        </div>
-                        <div className="row">
-                          <div className="col-xs-6 col-md-12 form-row">
-                            <button className="button button--primary button--block" type="submit">Submit</button>
-                          </div>
-                        </div>
-                        <div className="wpcf7-response-output" aria-hidden="true"></div>
-                      </form>
-                    </div>
-                  </div>
-              </div>
+              <Questions/>
             </aside>
           </div>
         </article>
@@ -77,11 +39,27 @@ export default function Deals({
   )
 }
 
+export default DealsPage
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
+  const instance = axios.create({
+    httpsAgent: new https.Agent({  
+      rejectUnauthorized: false
+    })
+  });
   
+  let config = {
+    method: 'get',
+    rejectUnauthorized: false,
+    maxBodyLength: Infinity,
+    url: `${process.env.apiDomain}/deals`,
+    headers: { }
+  };
+  const deals = await instance.request(config)
   return {
       props: {
-          messages: (await import(`../../../messages/${context.locale}.json`)).default
+        data: deals?.data,
+        messages: (await import(`../../../messages/${context.locale}.json`)).default
       },
   };
 };
