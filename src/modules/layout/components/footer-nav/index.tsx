bbecb17795fe7c2b33dc9d763b29991cb09c499f
luguumur <iluguumur@gmail.com>
2024-09-17 20:56:10 +0800

    up

--- src/modules/layout/components/footer-nav/index.tsx
+++ src/modules/layout/components/footer-nav/index.tsx
@@ -11,86 +11,86 @@ const FooterNav = () => {
   const menut = useTranslations("Menu");
   const homet = useTranslations("Home");
   return (
-    <footer className="page-footer " id="page-footer">
+    <div className="page-footer " id="page-footer">
       <div className="page-footer__main">
-          <div className="container">
-            <div className="row push--bottom">
-                <div className="col-md-7 col-sm-9">
-                  <nav className="nav--footer">
-                      <ul id="menu-main-navigation-1" className="menu">
-                        <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-104308"><Link href="/equipment/new">{menut("new")}</Link></li>
-                        <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-31"><Link href="/equipment/used">{menut("used")}</Link></li>
-                        <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-13463"><Link href="/equipment/rental">{menut("rent")}</Link></li>
-                        <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-4893"><Link href="/parts">{menut("parts")}</Link></li>
-                        <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-34"><Link href="/service">{menut("service")}</Link></li>
-                        <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-35"><Link href="/technology">{menut("technology")}</Link></li>
-                        <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-297"><Link href="/about-us">{menut("about")}</Link></li>
-                        <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-105841"><Link href="/careers">{menut("hr")}</Link></li>
-                      </ul>
-                  </nav>
-                </div>
-                <div className="col-md-5 col-sm-3">
-                  <ul className="page-footer__social social-links hidden-xs hidden-sm clearfix">
-                      <li><a className="icon-facebook" target="_blank" rel="nofollow" href="https://www.facebook.com/"></a></li>
-                      <li><a className="icon-twitter" target="_blank" rel="nofollow" href="https://twitter.com/"></a></li>
-                      <li><a className="icon-instagram" target="_blank" rel="nofollow" href="https://www.instagram.com/"></a></li>
-                      <li><a className="icon-linkedin2" target="_blank" rel="nofollow" href="https://www.linkedin.com/"></a></li>
-                      <li><a className="icon-youtube3" target="_blank" rel="nofollow" href="https://www.youtube.com/"></a></li>
-                  </ul>
-                  <a className="button phone-number" href="tel:+97670187588">+976 7018-7588</a>
-                </div>
+        <div className="container">
+          <div className="row push--bottom">
+            <div className="col-md-7 col-sm-9">
+              <nav className="nav--footer">
+                <ul id="menu-main-navigation-1" className="menu">
+                  <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-104308"><Link href="/equipment/new">{menut("new")}</Link></li>
+                  <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-31"><Link href="/equipment/used">{menut("used")}</Link></li>
+                  <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-13463"><Link href="/equipment/rental">{menut("rent")}</Link></li>
+                  <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-4893"><Link href="/parts">{menut("parts")}</Link></li>
+                  <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-34"><Link href="/service">{menut("service")}</Link></li>
+                  <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-35"><Link href="/technology">{menut("technology")}</Link></li>
+                  <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-297"><Link href="/about-us">{menut("about")}</Link></li>
+                  <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-105841"><Link href="/careers">{menut("hr")}</Link></li>
+                </ul>
+              </nav>
             </div>
-            <div className="row">
-              <div className="col-md-4 hidden-xxs hidden-xs hidden-sm">
-                <nav className="nav--footer-secondary clearfix">
-                  <ul id="menu-footer-menu-1" className="menu">
-                    <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-106276">
-                      <Link href="/careers" aria-current="page">{homet("workat")}</Link>
-                    </li>
-                    <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-106276">
-                      <Link href="/management" aria-current="page">{homet("board")}</Link>
-                    </li>
-                    <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-188">
-                      <Link href="/news?category=clx2mg66j0000mgwl9dou6ek8">{homet("social")}</Link>
-                    </li>
-                    <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-194">
-                      <Link href="/news">{homet("news")}</Link>
-                    </li>
-                    <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-196">
-                      <Link href="/magazine">{homet("magazine")}</Link>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-              <div className="col-sm-8 col-sm-push-4 col-md-4 col-md-push-4 col-lg-5 col-lg-push-3">
-                <div className="page-footer__extra-links clearfix">
-                  <div className="page-footer__google-translate"></div>
-                  <ul>
-                    <li>
-                      <Link href="/locations">Locations</Link>
-                    </li>
-                    <li className="hidden-xxs">
-                      <Link href="/contact-us">Contact Us</Link>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-sm-4 col-sm-pull-8 col-md-4 col-md-pull-4 col-lg-3 col-lg-pull-5">
-                <p className="page-footer__copyright"> Copyright © 2024 Barloworld Mongolia. <br /> All Rights Reserved </p>
-              </div>
+            <div className="col-md-5 col-sm-3">
+              <ul className="page-footer__social social-links hidden-xs hidden-sm clearfix">
+                <li><a className="icon-facebook" target="_blank" rel="nofollow" href="https://www.facebook.com/"></a></li>
+                <li><a className="icon-twitter" target="_blank" rel="nofollow" href="https://twitter.com/"></a></li>
+                <li><a className="icon-instagram" target="_blank" rel="nofollow" href="https://www.instagram.com/"></a></li>
+                <li><a className="icon-linkedin2" target="_blank" rel="nofollow" href="https://www.linkedin.com/"></a></li>
+                <li><a className="icon-youtube3" target="_blank" rel="nofollow" href="https://www.youtube.com/"></a></li>
+              </ul>
+              <a className="button phone-number" href="tel:+97670187588">+976 7018-7588</a>
             </div>
           </div>
+          <div className="row">
+            <div className="col-md-4 hidden-xxs hidden-xs hidden-sm">
+              <nav className="nav--footer-secondary clearfix">
+                <ul id="menu-footer-menu-1" className="menu">
+                  <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-106276">
+                    <Link href="/careers" aria-current="page">{homet("workat")}</Link>
+                  </li>
+                  <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-106276">
+                    <Link href="/management" aria-current="page">{homet("board")}</Link>
+                  </li>
+                  <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-188">
+                    <Link href="/news?category=clx2mg66j0000mgwl9dou6ek8">{homet("social")}</Link>
+                  </li>
+                  <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-194">
+                    <Link href="/news">{homet("news")}</Link>
+                  </li>
+                  <li className="menu-item menu-item-type-post_type menu-item-object-page menu-item-196">
+                    <Link href="/magazine">{homet("magazine")}</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className="col-sm-8 col-sm-push-4 col-md-4 col-md-push-4 col-lg-5 col-lg-push-3">
+              <div className="page-footer__extra-links clearfix">
+                <div className="page-footer__google-translate"></div>
+                <ul>
+                  <li>
+                    <Link href="/locations">Locations</Link>
+                  </li>
+                  <li className="hidden-xxs">
+                    <Link href="/contact-us">Contact Us</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-sm-4 col-sm-pull-8 col-md-4 col-md-pull-4 col-lg-3 col-lg-pull-5">
+              <p className="page-footer__copyright"> Copyright © 2024 Barloworld Mongolia. <br /> All Rights Reserved </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </footer>
+    </div>
   )
 }
 
 export default FooterNav
 
-export async function getStaticProps({locale}: GetStaticPropsContext) {
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
   return {
-      props: {
-          messages: (await import(`../../../../../messages/${locale}.json`)).default
-      }
+    props: {
+      messages: (await import(`../../../../../messages/${locale}.json`)).default
+    }
   };
 }
--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -1,16 +1,17 @@
-import FooterNav from "@/modules/layout/components/footer-nav"
+import FooterNav from "@/modules/layout/components/footer-nav";
 import { GetStaticPropsContext } from "next";
 import { useState } from "react";
 import { toast } from 'react-toastify';
 import NProgress from 'nprogress';
 import { GoogleReCaptchaProvider, useGoogleReCaptcha } from 'react-google-recaptcha-v3';
-
 import axios from 'axios';
 import { useTranslations } from "next-intl";
 import { useRouter } from "next/router";
+
 interface FormData {
   email: string;
 }
+
 const Footer = () => {
   const router = useRouter();
   const { executeRecaptcha } = useGoogleReCaptcha();
@@ -19,96 +20,102 @@ const Footer = () => {
 
   const initialData = {
     email: '',
-  }
+  };
+
   const [formData, setFormData] = useState<FormData>(initialData);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
-    setFormData((prevFormData) => ({
+    setFormData(prevFormData => ({
       ...prevFormData,
       [name]: value,
     }));
   };
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 
-    NProgress.start();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    NProgress.start();
 
     if (!executeRecaptcha) {
-      console.log("not available to execute recaptcha")
+      console.log("Recaptcha not available");
+      toast.error('Recaptcha service is unavailable.');
+      NProgress.done();
       return;
     }
-    const gRecaptchaToken = await executeRecaptcha('inquirySubmit');
-    const response = await axios({
-      method: "post",
-      url: "/api/recaptchaSubmit",
-      data: {
-        gRecaptchaToken,
-      },
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      },
-    });
 
-    if (response?.data?.success === true) {
-      // console.log(`Success with score: ${response?.data?.score}`);
-      try {
-        e.preventDefault();
-        const response = await axios.post(`/api/email`, {
+    try {
+      const gRecaptchaToken = await executeRecaptcha('inquirySubmit');
+      const recaptchaResponse = await axios.post("/api/recaptchaSubmit", {
+        gRecaptchaToken,
+      }, {
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (recaptchaResponse?.data?.success) {
+        const emailResponse = await axios.post(`/api/email`, {
+          email: formData.email,
+        }, {
           headers: {
-            'Content-Type': 'application/json', // Example header
+            'Content-Type': 'application/json',
           },
-          data: JSON.stringify({
-            "email": formData['email']
-          })
         });
-        if (response.status == 200) {
-          toast.success(`Амжилттай илгээгдлээ. Баярлалаа`);
-          NProgress.done();
+
+        if (emailResponse.status === 200) {
+          toast.success('Successfully submitted. Thank you!');
         } else {
-          toast.error(`Мэдээлэл олдохгүй байна.`);
-          NProgress.done();
+          toast.error('Failed to submit information.');
         }
-      } catch (error: any) {
-        // console.log(error)
-        toast.error(`Something went wrong`);
-        NProgress.done();
+      } else {
+        toast.error('Recaptcha validation failed.');
       }
-    } else {
-      console.log(`Failure with score: ${response?.data?.score}`);
-      toast.error(`Recaptcha error`);
+    } catch (error) {
+      toast.error('Something went wrong.');
+    } finally {
       NProgress.done();
     }
   };
 
   const home = useTranslations("Home");
+
   return (
     <footer>
-      {showFooter && <div className="container py-[30px]">
-        <div className="col-md-6 h-[48.65px] flex items-center ">
-          <span className="text-sm">{home("connect_your_email")}</span>
-        </div>
-        <form onSubmit={handleSubmit} className="wpcf7-form init" id="emailForm" aria-label="email form" data-status="init">
-          <div className="col-md-6">
-            <div className="row">
-              <div className="col-sm-6 form-field !pr-0">
-                <input type="email" name="email" value={formData.email} onChange={handleChange} className="wpcf7-form-control wpcf7-text wpcf7-validates-as-required h-[48.65px]" placeholder="Цахим хаяг" aria-required="true" aria-invalid="false" required />
-              </div>
-              <div className="col-sm-6 form-field">
-                <button className="button button--primary button--block" type="submit">Submit</button>
+      {showFooter && (
+        <div className="container py-[30px]">
+          <div className="col-md-6 h-[48.65px] flex items-center">
+            <span className="text-sm">{home("connect_your_email")}</span>
+          </div>
+          <form onSubmit={handleSubmit} className="wpcf7-form init" id="emailForm" aria-label="email form" data-status="init">
+            <div className="col-md-6">
+              <div className="row">
+                <div className="col-sm-6 form-field !pr-0">
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    className="wpcf7-form-control wpcf7-text wpcf7-validates-as-required h-[48.65px]"
+                    placeholder="Цахим хаяг"
+                    aria-required="true"
+                    required
+                  />
+                </div>
+                <div className="col-sm-6 form-field">
+                  <button className="button button--primary button--block" type="submit">Submit</button>
+                </div>
               </div>
             </div>
-          </div>
-        </form>
-      </div>
-      }
+          </form>
+        </div>
+      )}
       <FooterNav />
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
-
+export default Footer;
 
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
   return {
